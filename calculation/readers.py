db65260ace2b2014fee1e0dc93911a0c1d74b94d
Henrik <henrrb@stud.ntnu.no>
2022-12-30 00:46:26 +0100

    Set up structure of readers

--- calculation/readers.py
+++ calculation/readers.py
@@ -1,11 +1,16 @@
 #Functions:
 # - *Define readers ({exchange: <reader>})
 # - get_files
-# - find_reader
+# - get_reader
 # - read_file
 # - *initialize_exchange_readers?
 
-readers = {}
+readers = {
+
+}
+
+
+### Methods ###
 
 # Get uploaded files
 def get_files():
@@ -14,26 +19,60 @@ def get_files():
 
     return None
 
+# Initialize readers
+def initialize_readers():
+    
+    # Initialize readers-dictionary
+
+    return None
+
 # Get reader
 def get_reader(exchange):
 
     # Validations
-    if not isValidExchange(exchange):
-        return None                                         # TODO: Fix error handling
+    if not isValidExchange(exchange): return None                       # TODO: Fix error handling
 
     return readers[exchange]
 
 def read_file(reader, filedata):
 
     # Use reader to read filedata
-    # Return transactions
-    # TODO: Fix
+    transactions = reader.read_file(filedata)
 
-    return None
-
+    return transactions
 
 
-# Helper methods
+### Readers ###
+
+# BinanceReader
+class BinanceReader:
+
+    def read_file(filedata):
+        
+        # Read filedata
+        # Put data into transactions-format
+        # Return transactions
+        
+        return None
+
+# CoinbaseReader
+class CoinbaseReader:
+
+    def read_file(filedata):
+        
+        # Read filedata
+        # Put data into transactions-format
+        # Return transactions
+        
+        return None
+
+# ...
+
+
+
+
+### Helper methods ###
+
 def isValidExchange(exchange):
     for reader_exchange, reader in readers:
         if exchange == reader_exchange: return True
