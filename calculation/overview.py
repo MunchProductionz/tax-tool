ec874093d34bd03cbe8846d828a046119e0bf4e0
Henrik <henrrb@stud.ntnu.no>
2022-12-30 01:03:00 +0100

    Set up writer

--- calculation/overview.py
+++ calculation/overview.py
@@ -5,10 +5,12 @@
 # X Datastructures
 # / Priceretriever
 # X Calculation (Add features, fix TODOs)
+# Writer
 
 from readers import get_files
 from datacleaner import get_transactions_from_files
 from calculation import calculate_profit
+from writer import write_to_excel
 
 
 def run():
@@ -21,4 +23,5 @@ def run():
     files = get_files()
     transactions = get_transactions_from_files(files)
     amounts, transaction_profits, currency_profits = calculate_profit(transactions, order, fiat)
+    write_to_excel(transactions, transaction_profits, currency_profits)
 
---
+++ calculation/writer.py
@@ -0,0 +1,16 @@
+
+
+
+
+def write_to_excel(transactions, transaction_profits, currency_profits):
+
+    # Profit per transaction
+    # - Write each transaction to a new line in excel
+    # - Write each transaction_profit to a new line in excel
+    # TODO: Fix
+
+    # Assets and total profit per currency
+    # - Write each transaction to a new line in excel (debit/credit per currency)
+    # - Write currency_profit per currency
+
+    return None
