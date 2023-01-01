1529091098440d2d57c26eaa3c0e3a9bd1511fb3
Henrik <henrrb@stud.ntnu.no>
2023-01-01 12:03:13 +0100

    Initialize workbook and worksheets

--- calculation/writer.py
+++ calculation/writer.py
@@ -1,8 +1,14 @@
+import xlsxwriter
 
+# Cleaned transactions: [transaction1, transaction2, ...]
+# Cleaned transaction: [date, currency_sold, amount_sold, price_sold, currency_bought, amount_bought, price_bought]
 
-
+# Transaction_profits: {[currency]: [date, profit], ...}
+# Currency_profits: {[currency]: profit, ...}
 
 def write_to_excel(transactions, transaction_profits, currency_profits):
+
+    workbook, transactions, assets = initialize_workbook_and_worksheets()
 
     # Profit per transaction
     # - Write each transaction to a new line in excel
@@ -14,3 +20,12 @@ def write_to_excel(transactions, transaction_profits, currency_profits):
     # - Write currency_profit per currency
 
     return None
+
+
+### Helper Methods ###
+def initialize_workbook_and_worksheets():
+    workbook = xlsxwriter.Workbook('tax_info.xlsx')
+    transactions = workbook.add_worksheet('Transactions')
+    assets = workbook.add_worksheet('Assets')
+    
+    return workbook, transactions, assets
