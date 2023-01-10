2421b72cc25c71ef8bd60d57f96e068da0544b3a
Henrik <henrrb@stud.ntnu.no>
2023-01-10 14:10:28 +0100

    Define CoinbaseReader (fix comma-error when reading)

--- calculation/readers.py
+++ calculation/readers.py
@@ -92,7 +92,7 @@ class BinanceReader:
                 amount_bought = cleaned_data["Total"].loc[row]
                 
                 currency_sold = second_currency
-                price_sold = 1 / price_sold
+                price_sold = 1 / price_bought
                 amount_sold = cleaned_data["Amount"].loc[row]
             
             transaction = [date_formatted, currency_sold, amount_sold, price_sold, currency_bought, amount_bought, price_bought]
@@ -109,11 +109,48 @@ class CoinbaseReader:
 
     def read_file(self, file_path):
         
+        time_format = get_time_format()
+        
         # CSV
-        uncleaned_data = pd.read_csv(r'' + file_path + '')
-        print(uncleaned_data.head())
-    
+        uncleaned_data = pd.read_csv(r'' + file_path + '', delimiter='|', quotechar='"', skiprows=7)
+        
         # cleaned_data = uncleaned_data[["Date(UTC)"]]
+        cleaned_data = uncleaned_data[["Timestamp", "Transaction Type", "Asset", "Quantity Transacted", "USD Spot Price at Transaction", "USD Subtotal"]]
+        
+        print(cleaned_data.head())
+        
+        
+        transactions = []
+        
+        for row in range(len(cleaned_data)):
+            date = cleaned_data["Date"].loc[row]
+            date_formatted = date.strftime(time_format)
+            type = cleaned_data["Transaction Type"].loc[row]
+            note = cleaned_data["Notes"].loc[row]
+            second_currency = get_currency_from_end_of_note(note)
+            
+            if type == 'SELL':
+                currency_sold = cleaned_data["Asset"].loc[row]
+                price_sold = cleaned_data["USD Spot Price at Transaction"].loc[row]
+                amount_sold = cleaned_data["Quantity Transacted"].loc[row]
+                
+                currency_bought = second_currency
+                price_bought = 1 / price_sold
+                amount_bought = get_amount_from_end_of_note(note)
+            elif type == 'BUY':
+                currency_bought = cleaned_data["Asset"].loc[row]
+                price_bought = cleaned_data["USD Spot Price at Transaction"].loc[row]
+                amount_bought = cleaned_data["Quantity Transacted"].loc[row]
+                
+                currency_sold = second_currency
+                price_sold = 1 / price_bought
+                amount_sold = get_amount_from_end_of_note(note)
+            else:
+                break
+            
+            transaction = [date_formatted, currency_sold, amount_sold, price_sold, currency_bought, amount_bought, price_bought]
+            transactions.append(transaction)
+
 
         # Read filedata
         # Put data into transactions-format
@@ -176,16 +213,39 @@ def get_first_currency(market):
     
     return first_currency    # TODO: Handle when ticker is invalid
 
+def get_currency_from_end_of_note(note):
+    
+    currencies = get_currencies()
+    currency = None
+    
+    for ticker in range(len(note)-1, 1, -1):
+        if note[ticker:] in currencies:
+            currency = note[ticker:]
+    
+    return currency    # TODO: Handle when ticker is invalid
+
+def get_amount_from_end_of_note(note):
+    
+    currency = get_currency_from_end_of_note(note)
+    amount = 0
+    
+    for number in range(len(note)-len(currency)-1, 1, -1):
+        if note[number:number + 1] == " ":
+            amount = int(note[number:len(note)-len(currency)-1])
+            
+    return amount
+
 def get_time_format():
     
     return '%Y-%m-%d %X'
+
 
 ### Testing ###
 
 files = get_files()
 readers = get_readers()
-exchange = files[0][0]
-file_path = files[0][1]
+exchange = files[1][0]
+file_path = files[1][1]
 print(files)
 print(file_path)
 read_file(readers[exchange], file_path)
