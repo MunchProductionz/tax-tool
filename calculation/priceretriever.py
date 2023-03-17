58e58e923503f7c3a76fe12339f520feae0ba08b
Henrik <henrrb@stud.ntnu.no>
2023-03-17 07:53:28 +0100

    Implement get_USD_to_fiat_conversion_rate and get_price

--- calculation/priceretriever.py
+++ calculation/priceretriever.py
@@ -4,6 +4,7 @@ import pandas as pd
 from variables import currencies_full_names
 from variables import convert_values_to_lowercase
 from variables import days_in_months
+from variables import isValidCurrency
 
 def get_average_price_crypto(date, ticker):
     
@@ -12,8 +13,8 @@ def get_average_price_crypto(date, ticker):
     currency = currencies_full_names_lowercase[ticker]
     
     # Get start and end date
-    start_date = get_date_string(date)
-    end_date = get_next_date_string(start_date)
+    start_date = get_start_date_string(date)
+    end_date = get_end_date_string(start_date)
     
     # Send a GET request
     url = "https://www.coingecko.com/en/coins/" + currency + "/historical_data/?start_date=" + start_date + "&end_date=" + end_date + "#panel"
@@ -31,88 +32,111 @@ def get_average_price_crypto(date, ticker):
     # Clean the data
     opening_price_usd = df_table[0]["Open"][1]
     closing_price_usd = df_table[0]["Close"][1]
-    print('Open: ' + str(opening_price_usd))
-    print('Close: ' + str(closing_price_usd))
-    
     
     # Calculate average daily price
     average_price = get_average_price(opening_price_usd, closing_price_usd)
-    print('Average: ' + str(average_price))
     
     return average_price
 
-def get_fiat_to_USD_conversion_rate(date, fiat):
+def get_USD_to_fiat_conversion_rate(date, fiat):
     
-    return None
+    # Get fiat currency
+    fiat_currency = currencies_full_names[fiat]
+    
+    # Send a GET request
+    url = "https://www.x-rates.com/historical/?from=USD&amount=1&date=" + date
+    response = requests.get(url)
+    
+    # Parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Find the table containing the historical price data
+    table = soup.find('table', {'class': "tablesorter ratesTable"})
+
+    # Convert to pandas dataframe
+    df_table = pd.read_html(str(table))[0]
+    
+    # Clean the data
+    df_table_currency = df_table['US Dollar']
+    df_table = df_table[(df_table_currency == fiat_currency)]
+    USD_to_fiat_conversion_rate = df_table["1.00 USD"].iloc[0]
+    
+    return USD_to_fiat_conversion_rate
 
 
 def get_price(date, ticker, fiat):
+
+    # TODO: Add actual error handling
+    # Error handling
+    if not isValidCurrency(ticker): return None
+    if not isValidCurrency(fiat): return None
 
     # Get average USD price of currency
     average_usd_price_crypto = get_average_price_crypto(date, ticker)
 
     # Get fiat to USD conversion rate at date
-    fiat_to_USD_conversion_rate = get_fiat_to_USD_conversion_rate(date, fiat)
+    USD_to_fiat_conversion_rate = get_USD_to_fiat_conversion_rate(date, fiat)
 
     # Get fiat_price of currency on input date
-    fiat_price = average_usd_price_crypto * fiat_to_USD_conversion_rate
+    fiat_price = average_usd_price_crypto * USD_to_fiat_conversion_rate
+    print(f'{fiat}: {str(fiat_price)}')
 
-    return None
+    return fiat_price
 
 
 ### Helper Methods ###
 
-def get_date_string(date):
+def get_start_date_string(date):
     
     # Example:
     # - date = 2022-11-15
-    # - date_string = 20221115
+    # - start_date_string = 20221115
     
     date_string = date.replace("-", "")
     
     return date_string
 
-def get_next_date_string(date_string):
+def get_end_date_string(date_string):
     
     # Example:
     # - date_string = 20221115
-    # - next_date_string = 20221116
+    # - end_date_string = 20221116
 
     year = date_string[:4]
     month = date_string[4:6]
     day = date_string[6:]
 
-    if isNewYear(date_string, month, day):
+    if isNewYear(month, day):
         next_year = str(int(year) + 1)
         next_month = '01'
         next_day = '01'
-        next_date_string = next_year + next_month + next_day
-    elif isNewMonth(date_string, month, day):
+        end_date_string = next_year + next_month + next_day
+    elif isNewMonth(month, day):
         if int(month) < 9:
             next_month = '0' + str(int(month) + 1)
         else:
             next_month = str(int(month) + 1)
         next_day = '01'
-        next_date_string = year + next_month + next_day
+        end_date_string = year + next_month + next_day
     else:
         if int(day) < 9:
             next_day = '0' + str(int(day) + 1)
         else:
             next_day = str(int(day) + 1)
-        next_date_string = year + month + next_day
+        end_date_string = year + month + next_day
         
-    return next_date_string
+    return end_date_string
 
-def isNewYear(date_string, month, day):
+def isNewYear(month, day):
     
-    if isNewMonth(date_string, month, day):
+    if isNewMonth(month, day):
         if month == '12':
             print('New Year')
             return True
         
     return False
 
-def isNewMonth(date_string, month, day):
+def isNewMonth(month, day):
     
     if day == days_in_months[month]:
         return True
@@ -128,7 +152,7 @@ def get_average_price(opening_price_string, closing_price_string):
 
 def get_price_int(price_string):
     
-    # TODO: Fix to fully accoutn for floats (it currently removes everything after '.')
+    # TODO: Fix to fully account for floats (it currently removes everything after '.')
     price_string_no_dollar_sign = price_string.replace('$', '')
     price_string_no_comma = price_string_no_dollar_sign.replace(',', '')
     if '.' in price_string_no_comma:
@@ -138,9 +162,9 @@ def get_price_int(price_string):
     return price_int
 
 
-# print(get_date_string(date))
-
-date = "2023-02-28"
+date = "2023-02-15"
 date_string = "20230228"
 
-get_average_price_crypto(date, "LTC")
+# get_average_price_crypto(date, "LTC")
+# get_USD_to_fiat_conversion_rate(date, 'NOK')
+get_price(date, "BTC", "NOK")
--- calculation/variables.py
+++ calculation/variables.py
@@ -1,8 +1,67 @@
+### Variables ###
 
 currencies_full_names = {
+    
+    # Cryptocurrencies
     'BTC': 'Bitcoin',
     'ETH': 'Ethereum',
     'LTC': 'Litecoin',
+    
+    # Fiat currencies
+    'USD': 'US Dollar',
+    'EUR': 'Euro',
+    'GBP': 'British Pound',
+    'NOK': 'Norwegian Krone',
+    'INR': 'Indian Rupee',
+    'AUD': 'Australian Dollar',
+    'CAD': 'Canadian Dollar',
+    'SGD': 'Singapore Dollar',
+    'CHF': 'Swiss Franc',
+    'MYR': 'Malaysian Ringgit',
+    'JPY': 'Japanese Yen',
+    'CNY': 'Chinese Yuan Renminbi',     # Uses RMB for domestic transactions
+    'ARS': 'Argentine Peso',
+    'BHD': 'Bahraini Dinar',
+    'BWP': 'Botswana Pula',
+    'BRL': 'Brazilian Real',
+    'BND': 'Bruneian Dollar',
+    'BGN': 'Bulgarian Lev',
+    'CLP': 'Chilean Peso',
+    'COP': 'Colombian Peso',
+    'CZK': 'Czech Koruna',
+    'DKK': 'Danish Krone',
+    'AED': 'Emirati Dirham',
+    'HKD': 'Hong Kong Dollar',
+    'HUF': 'Hungarian Forint',
+    'ISK': 'Icelandic Krona',
+    'IDR': 'Indonesian Rupiah',
+    'IRR': 'Iranian Rial',
+    'ILS': 'Israeli Shekel',
+    'KZT': 'Kazakhstani Tenge',
+    'KWD': 'Kuwaiti Dinar',
+    'LYD': 'Libyan Dinar',
+    'MUR': 'Mauritian Rupee',
+    'MXN': 'Mexican Peso',
+    'NPR': 'Nepalese Rupee',
+    'NZD': 'New Zealand Dollar',
+    'OMR': 'Omani Rial',
+    'PKR': 'Pakistani Rupee',
+    'PHP': 'Philippine Peso',
+    'PLN': 'Polish Zloty',
+    'QAR': 'Qatari Riyal',
+    'RON': 'Romanian New Leu',
+    'RUB': 'Russian Ruble',
+    'SAR': 'Saudi Arabian Riyal',
+    'ZAR': 'South African Rand',
+    'KRW': 'South Korean Won',
+    'LKR': 'Sri Lankan Rupee',
+    'SEK': 'Swedish Krona',
+    'TWD': 'Taiwan New Dollar',
+    'THB': 'Thai Baht',
+    'TTD': 'Trinidadian Dollar',
+    'TRY': 'Turkish Lira',
+    'VEF': 'Venezuelan Bolivar'
+    
 }
 
 days_in_months = {
@@ -20,6 +79,9 @@ days_in_months = {
     '12': '31'
 }
 
+
+### Methods ###
+
 def convert_values_to_lowercase(dictionary):
     currencies_full_names_small_letters = dict()
     
@@ -28,3 +90,7 @@ def convert_values_to_lowercase(dictionary):
         
     return currencies_full_names_small_letters
 
+def isValidCurrency(currency):
+    if currency in currencies_full_names:
+        return True
+    return False
